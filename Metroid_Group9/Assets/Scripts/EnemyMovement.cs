c01b6cb3f8775050137f9c0ac607f39769bc948e
SDR1109 <sdr1109v@gmail.com>
2025-04-04 17:35:51 -0700

    EnemyMovement script added

---
+++ Metroid_Group9/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+/*Author [Samuel Diaz ]
+ * Last Updated [04/04/2025]
+ * Description [This code is in charge of the enemy movement]
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
