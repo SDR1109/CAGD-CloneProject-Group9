6d78b623b14da02cea9db5486f0bd79108bcd0ee
SDR1109 <sdr1109v@gmail.com>
2025-04-08 15:48:28 -0700

    Portal Script finished and working

--- Metroid_Group9/Assets/Scripts/PortalScript.cs
+++ Metroid_Group9/Assets/Scripts/PortalScript.cs
@@ -1,18 +1,18 @@
+/*Author [Samuel Diaz ]
+ * Last Updated [04/04/2025]
+ * Description [This code is in charge of the portal]
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform TeleportPoint;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        other.transform.position = TeleportPoint.position;
     }
 }
