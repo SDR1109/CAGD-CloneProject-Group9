a5c065fdff3b107515b9ebe5053b60f7692fac48
Blaze-Hollow <blazehollow718@gmail.com>
2025-04-08 17:57:52 -0700

    Shooting Mechanic WIP
    Made a bullet script and added to the "Shoot" function in the player prefab

---
+++ Metroid_Group9/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+/*
+ * Name: Maya Andrade
+ * Date: 04/08/25
+ * Last Updated: 04/08/25
+ * Description: Allows player's bullets to damage enemies and despawn
+ */
+
+public class BulletScript : MonoBehaviour
+{
+    public int bulletDamage = 1;
+
+    //regular bullet deals 1HP of damage
+    //heavy bullet deals 3HP of damage
+
+    public int bulletSpeed = 10;
+    public int bulletDespawnTime = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start timer to despawn bullet if it doesn't hit anything
+        StartCoroutine(DespawnTime());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Despawns the bullet after x amount of seconds
+    /// </summary>
+    IEnumerator DespawnTime()
+    {
+        yield return new WaitForSeconds(bulletDespawnTime);
+        Destroy(gameObject);
+    }
+}
--- Metroid_Group9/Assets/Scripts/Player.cs
+++ Metroid_Group9/Assets/Scripts/Player.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /*
  * Name: Maya Andrade
  * Date: 04/06/25
- * Last Updated: 04/07/25
- * Description: Allows player movement and combat control
+ * Last Updated: 04/08/25
+ * Description: Allows player movement
  */
 public class Player : MonoBehaviour
 {
@@ -17,7 +19,7 @@ public class Player : MonoBehaviour
     public float jumpForce = 7f;
     //makes it so that you can check under the player for ground in OnGround() incase the model height is changed later on
     public float underPlayer = 1.2f;
-    public bool isfacingLeft;
+    private bool isfacingLeft;
 
     private Rigidbody rb;
 
@@ -27,8 +29,12 @@ public class Player : MonoBehaviour
     public int blinkCounter = 5;
 
     [Header("Combat Variables")]
-    public GameObject regBullets;
-    public GameObject heavyBullets;
+    public GameObject regularBullet;
+    public GameObject heavyBullet;
+    private bool bulletUpgraded = false;
+    public bool canShoot = true;
+    public int magazineSize = 2;
+    public int bulletsShot = 0;
 
 
 
@@ -44,6 +50,18 @@ public class Player : MonoBehaviour
     {
         Move();
         Jump();
+        Shoot();
+
+        if (healthPoints <= 0) //checks to see if player has "died" yet or not
+        {
+            SceneManager.LoadScene(3);
+            print("GAME OVER =[");
+        }
+        if (bulletsShot >= magazineSize)  //checks to see if the player needs a cooldown time from shooting bullets
+        {
+            canShoot = false;
+            StartCoroutine(BulletCooldown());
+        }
     }
 
     /// <summary>
@@ -55,7 +73,7 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) 
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-1, 1, 1); 
             isfacingLeft = true;
         }
 
@@ -100,25 +118,6 @@ public class Player : MonoBehaviour
     }
 
     /// <summary>
-    /// Make player lose HP, blink when they do so, and goes to game over screen if they reach 0HP
-    /// </summary>
-    public void LoseHP()
-    {
-        //somehow get the player health to drop by the amount of damage the enemy type does
-
-        if (healthPoints > 0)
-        {
-            //The player should blink for the next 5 seconds and shouldn’t be able to get damaged again during that time. 
-            StartCoroutine(Blink());
-        }
-        else //hp is less than or equal to 0
-        {
-            SceneManager.LoadScene(3);
-            print("GAME OVER =[");
-        }
-    }
-
-    /// <summary>
     /// Makes the player blink when it takes damage
     /// </summary>
     private IEnumerator Blink()
@@ -143,6 +142,7 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Shoot()
     {
+
         /*
          * This will allow the player to shoot a new bullet in the direction the player is facing, 
          * while the player holds the fire button. 
@@ -151,32 +151,45 @@ public class Player : MonoBehaviour
          * OR LEFT ARROW fires left – RIGHT ARROW fires right.
          */
 
-
-        //if using arrow keys
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-
-        }
-
         //if using space bar
-        if (isfacingLeft)
+        if (Input.GetKey(KeyCode.Space))
         {
-            //shoot the bullets to the left
+            if (isfacingLeft == true)
+            {
+                if (bulletUpgraded == false)
+                {
+                    Instantiate(regularBullet, transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(heavyBullet, transform.position, Quaternion.identity);
+                }
+            }
+            else
+            {
+                if (bulletUpgraded == false)
+                {
+                    Instantiate(regularBullet, transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(heavyBullet, transform.position, Quaternion.identity);
+                }
+            }
         }
-        else
-        {
-            //shoot the bullets to the right
-        }
 
+       
+    }
 
-
-
-        //regular bullet deals 1HP of damage
-        //heavy bullet deals 3HP of damage
+    /// <summary>
+    /// Resets bullets shot and makes a cooldown where the player cannot shoot
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator BulletCooldown()
+    {
+        yield return new WaitForSeconds(1);
+        canShoot = true;
+        bulletsShot = 0;
     }
 
 
